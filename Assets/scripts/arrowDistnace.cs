b765fe9e11790392c95b1ff4d9c4bf177fccc1a8
Alejandro Nicolas Biga <alejandro.z.biga@gmail.com>
2023-12-06 10:53:27 -0300

    distancias medidas

---
+++ Assets/scripts/arrowDistnace.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class arrowDistnace : MonoBehaviour
+{
+    public string targetTag = "Target"; 
+    private Transform target;
+    public TextMeshProUGUI distanceText;
+
+    private void Start()
+    {
+        
+        GameObject targetObject = GameObject.FindGameObjectWithTag(targetTag);
+
+        if (targetObject != null)
+        {
+            
+            target = targetObject.transform;
+        }
+        
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            
+            float distance = Vector3.Distance(transform.position, target.position);
+
+            
+            Debug.Log($"Distancia entre {gameObject.name} y {target.name}: {distance}");
+
+            
+            if (distanceText != null)
+            {
+                distanceText.text = $"Distancia: {distance.ToString("F2")} unidades";
+            }
+            
+        }
+    }
+}
