f7155cabdc574cb5c4a878b2f317db3b80e7ca1f
Thomás Alonso <96887716+HyperSpaceGZ@users.noreply.github.com>
2023-12-15 11:40:09 -0300

    segso

---
+++ Assets/BowShooting.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BowShooting : MonoBehaviour
+{
+    public float ArrowForce;
+    public float objectLifetime = 3;
+    public GameObject Arrow;
+    public Transform ArrowSpawner;
+    public Slider ForceSlider;
+    public TextMeshProUGUI ForceText;
+    public void Update()
+    {
+        ArrowForce = ForceSlider.value;
+        ForceText.text = "" + ForceSlider.value;
+    }
+
+    public void ShootArrow()
+    {
+        GameObject ArrowClone = Instantiate(Arrow, ArrowSpawner.position, ArrowSpawner.rotation);
+        Rigidbody ArrowRB = ArrowClone.GetComponent<Rigidbody>();
+        ArrowRB.AddRelativeForce(Vector3.up * ArrowForce, ForceMode.Impulse);
+        StartCoroutine(DeactivateObject(ArrowClone));
+    }
+
+    IEnumerator DeactivateObject(GameObject objToDeactivate)
+    {
+        yield return new WaitForSeconds(objectLifetime);
+        Destroy(objToDeactivate);
+    }
+}
---
+++ Assets/Manager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+    public GameObject cam3;
+    public GameObject cam4;
+
+    private void Start()
+    {
+        SwitchCam1();
+    }
+
+    public void SwitchCam1()
+    {
+        cam1.SetActive(true);
+        cam2.SetActive(false);
+        cam3.SetActive(false);
+        cam4.SetActive(false);
+    }
+    public void SwitchCam2()
+    {
+        cam1.SetActive(false);
+        cam2.SetActive(true);
+        cam3.SetActive(false);
+        cam4.SetActive(false);
+    }
+    public void SwitchCam3()
+    {
+        cam1.SetActive(false);
+        cam2.SetActive(false);
+        cam3.SetActive(true);
+        cam4.SetActive(false);
+    }
+    public void SwitchCam4()
+    {
+        cam1.SetActive(false);
+        cam2.SetActive(false);
+        cam3.SetActive(false);
+        cam4.SetActive(true);
+    }
+}
