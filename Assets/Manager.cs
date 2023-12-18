7c9db61775316f50f882ac6cdac6276d35db4184
Alejandro Nicolas Biga <alejandro.z.biga@gmail.com>
2023-12-18 11:34:45 -0300

    PRIMERA BUILD

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -49,3 +50,4 @@ public class Manager : MonoBehaviour
         SceneManager.LoadScene("Arco1");
     }
 }
+#endif
